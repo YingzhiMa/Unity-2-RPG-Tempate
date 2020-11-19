83b7bd2496ddd0ebc574530d92c5d3fb7b1449b2
YingzhiMa <yingzhima1219@gmail.com>
2020-11-18 18:46:24 -0800

    Started project

---
+++ Assets/Main/Scripts/ScriptableObjectsCode/Dialogue/DialogueBranch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Branch", menuName="CustomObject/Branch")]
+public class DialogueBranch : ScripatbleObject
+{
+    public string dialogueId ="";
+    public List<striing>DialogueLines;
+    public List<ResponseBranch> ResponseOption;
+}
+
+[System.Serializabble]
+pulic class ResponseBranch
+{
+    public string text;
+    publlic DialogueBranch nextbranch;
+}
